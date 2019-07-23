1895f2a793f9dd563fdcd61c680f8145adb6cf84
ChaiUrs <mysorechaitra@gmail.com>
2019-07-23 17:02:03 +0000

    Implemented Equal Arrays

--- eqArrays.js
+++ eqArrays.js
@@ -9,14 +9,14 @@ const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
     return false;
   }
-  for (var i = 0; i < array1.length; i++) {
+  for (let i = 0; i < array1.length; i++) {
     if (array1 [i] !== array2[i]) {
       return false;
     }
   }
   return true;
-}
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); 
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); 
+};
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
