50fea892ad53842b3fc4ba8918f73b6626b53b6a
ChaiUrs <mysorechaitra@gmail.com>
2019-07-23 00:40:56 +0000

    Assert Equal Assignment completed

---
+++ assertEqual.js
@@ -0,0 +1,10 @@
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅ Assertion Passed :" + actual + "===" + expected);
+  } else {
+    console.log("🛑🛑🛑 Assertion Failed :" + actual + "!==" + expected);
+  }
+};
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
