9f73a63b12075af027b2a8d20c762500619db0d6
ChaiUrs <mysorechaitra@gmail.com>
2019-07-24 22:31:09 +0000

    Implemented counting letters

---
+++ countLetters.js
@@ -0,0 +1,27 @@
+/*
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed : ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed : ${actual} !== ${expected}`);
+  }
+}; */
+
+// count letters in a string and get an object to display the letters count for each word.
+const countLetters = function(string) {
+  let output_letters = {};
+  let words = string.split(' ');
+  for (let i = 0; i < words.length; i++) {
+    output_letters[words[i]] = {};
+    for (let j = 0; j < words[i].length; j++) {
+      if (output_letters[words[i]] [words[i][j]]) {
+      output_letters[words[i]][words[i][j]] ++;
+    } else {
+      output_letters[words[i]][words[i][j]] = 1;
+    }
+  }
+}
+return output_letters;
+}
+console.log(countLetters("Counting Letters Here"));
+// assertEqual(countLetters("wordcount"), 8);
