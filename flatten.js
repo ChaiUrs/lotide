63ab0127aba029a59be7f80bcc41a682801c3ace
ChaiUrs <mysorechaitra@gmail.com>
2019-07-24 19:01:39 +0000

    Implemented flatten

--- flatten.js
+++ flatten.js
@@ -1,4 +1,4 @@
-/*
+
 const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
     return false;
@@ -20,14 +20,16 @@ const assertArraysEqual = function(array1, array2) {
       console.log("Arrays are same!");
     }
 };
-*/
+
 const flatten = function(array1) {
 let flattened = [];
 for(let i = 0; i < array1.length; i++) {
   if (Array.isArray(array1[i])) {
   // The Array.isArray() method determines whether the passed value is an Array.
+
     flattened = flattened.concat(flatten(array1[i]));
   // Flattening nested arrays is equivalent to concatenating each of them to an empty array.
+  
   } else {
     flattened.push(array1[i]);
   }
@@ -35,4 +37,4 @@ for(let i = 0; i < array1.length; i++) {
 return flattened;
 };
 console.log(flatten([1, 2, [3, 4], 5, [6]]));
-// console.log(assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]),[1,2,3,4,5,6]));
+console.log(assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]),[1,2,3,4,5,6]));
