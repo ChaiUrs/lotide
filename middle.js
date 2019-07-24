ed9d47bb39595b81762f46fd815dcb9faf8140e0
ChaiUrs <mysorechaitra@gmail.com>
2019-07-24 01:32:35 +0000

    Implemented middle

---
+++ middle.js
@@ -0,0 +1,41 @@
+/*
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1 [i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) === true) {
+    console.log(`✅ Assertion Passed : ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed : ${actual} !== ${expected}`);
+  }
+};
+*/
+const middle = function(array) {
+  let array_result = [];
+  if (array.length <= 2 && array.length > 0) {
+    // For arrays with one or two elements, there is no middle. Return an empty array.
+    return array_result;
+  } else if (array.length % 2 === 0) {
+      // For arrays with an even number of elements, an array containing the two elements in the middle should be returned
+      array_result = [array[(array.length / 2) - 1], array[array.length / 2]];
+    } else {
+      // For arrays with odd number of elements, an array containing a single middle element should be returned.
+      array_result = array[(array.length - 1) / 2];
+    }
+    return array_result;
+};
+console.log(middle([1]));
+console.log(middle([1, 2]));
+console.log(middle([1, 2, 3]));
+console.log(middle([1, 2, 3, 4, 5]));
+console.log(middle([1, 2, 3, 4]));
+console.log(middle([1, 2, 3, 4, 5, 6]));
