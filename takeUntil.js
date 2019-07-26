ce25e58d852e631afd29027573da1b5a68316218
ChaiUrs <mysorechaitra@gmail.com>
2019-07-26 01:00:19 +0000

    Implemented take until

---
+++ takeUntil.js
@@ -0,0 +1,46 @@
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1 [i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected) === true) {
+    console.log(`✅ Assertion Passed : ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed : ${actual} !== ${expected}`);
+  }
+};
+
+/* Two Parameters */
+// The array to work with
+// The callback (which Lodash calls "predicate")
+
+const takeUntil = function(array, callback) {
+  let newArray = [];
+  for (const item of array) {
+    if (!(callback(item))) {
+      newArray.push(item);
+    } else {
+      return newArray;
+    }
+  }
+  return newArray;
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
