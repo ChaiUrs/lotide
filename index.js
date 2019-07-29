618c1fa4f6e7ff1bc5b9850831103f4156f099fd
ChaiUrs <mysorechaitra@gmail.com>
2019-07-29 22:00:27 +0000

    index exporting object for functions head, tail and middle

---
+++ index.js
@@ -0,0 +1,10 @@
+
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+
+module.exports = {
+  head:   head,
+  tail:   tail,
+  middle: middle
+};
