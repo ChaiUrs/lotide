01b34c882ce606b3bf7d2203bf24eb1cb0cc0183
ChaiUrs <mysorechaitra@gmail.com>
2019-07-29 21:32:56 +0000

    Refactored middle with mocha and chai paired with @sockbot

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,9 @@
 
-const assertArraysEqual = require('../assertArraysEqual');
-const middle = require('../middle');
 
+const assert = require('chai').assert;
+const middle   = require('../middle');
 describe("#middle", () => {
   it("should return [2, 3] for [1, 2, 3, 4]", () => {
-    assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
   });
 });
