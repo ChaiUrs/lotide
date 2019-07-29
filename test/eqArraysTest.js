a15a1c7123c6ba3830894e6bfdb5abae9e736eff
ChaiUrs <mysorechaitra@gmail.com>
2019-07-29 06:06:53 +0000

    Refactoring equal arrays completed

---
+++ test/eqArraysTest.js
@@ -0,0 +1,8 @@
+
+const assertEqual = require('../assertEqual');
+const eqArrays = require('../eqArrays');
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
