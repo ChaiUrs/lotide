0ed942c25fadaabd1dfced3a5cb4c72c32fb0cc2
ChaiUrs <mysorechaitra@gmail.com>
2019-07-29 06:05:45 +0000

    Refactored equal arrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,4 +1,4 @@
-
+/*
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅ 💁‍♀️ Assertion Passed : ${actual} === ${expected}`);
@@ -6,6 +6,7 @@ const assertEqual = function(actual, expected) {
     console.log(`🛑 🙆‍♀️ Assertion Failed : ${actual} !== ${expected}`);
   }
 };
+*/
 const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
     return false;
@@ -17,7 +18,7 @@ const eqArrays = function(array1, array2) {
   }
   return true;
 };
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3]));
+
+module.exports = eqArrays;
