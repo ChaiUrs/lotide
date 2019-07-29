7a5e08f1b275e6cc1ccb57ea30599a8666d16b48
ChaiUrs <mysorechaitra@gmail.com>
2019-07-29 05:34:42 +0000

    Refactor Example completed

---
+++ test/assertEqualTest.js
@@ -0,0 +1,5 @@
+
+const assertEqual = require('../assertEqual');
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
---
+++ test/headTest.js
@@ -0,0 +1,8 @@
+
+const assertEqual = require('../assertEqual');
+const head = require('../head');
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([26]), 26);
+assertEqual(head([]), undefined);
