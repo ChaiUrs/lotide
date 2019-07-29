63d5138515d13db9a52a762646006520ff49d376
ChaiUrs <mysorechaitra@gmail.com>
2019-07-29 21:33:41 +0000

    Refactored middle

--- middle.js
+++ middle.js
@@ -33,11 +33,5 @@ const middle = function(array) {
       return array_result = array[(array.length - 1) / 2];
     }
 };
-console.log(middle([1]));
-console.log(middle([1, 2]));
-console.log(middle([1, 2, 3]));
-console.log(middle([1, 2, 3, 4, 5]));
-console.log(middle([1, 2, 3, 4]));
-console.log(middle([1, 2, 3, 4, 5, 6]));
 
 module.exports = middle;
