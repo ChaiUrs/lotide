17f2fcc42db5ebf314b9a92e9c2b9ace9e5254f5
ChaiUrs <mysorechaitra@gmail.com>
2019-07-29 06:45:38 +0000

    Refactored assert arrays equal

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,4 +1,4 @@
-
+/*
 const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
     return false;
@@ -9,16 +9,20 @@ const eqArrays = function(array1, array2) {
     }
   }
   return true;
-};
+}; */
 
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected) === true) {
-    console.log(`✅ Assertion Passed : ${actual} === ${expected}`);
+const assertArraysEqual = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return (`🛑 Assertion Failed : ${array1} !== ${array2}`);
   } else {
-    console.log(`🛑 Assertion Failed : ${actual} !== ${expected}`);
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return (`🛑 Assertion Failed : ${array1} !== ${array2}`);
+      }
+    }
   }
+  return (`✅ Assertion Passed : ${array1} === ${array2}`);
 };
-assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertArraysEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+console.log(assertArraysEqual([1, 2, 3], [1, 2, 3]));
+
+module.exports = assertArraysEqual;
